51ec4a0319e4971429081d9e00365a42122d7a47
davide-rullo <davide.rullo01@gmail.com>
2023-10-12 15:04:48 +0200

    add html and css structure

--- index.php
+++ index.php
@@ -6,11 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
+    <header>
+        <img src="./assets/img/spotify_logo.png" width="40" alt="">
+    </header>
+    <main>
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col">
+                    <div class="card">Lorem ipsum dolor sit amet consectetur.</div>
+                </div>
+                <div class="col">
+                    <div class="card">Lorem ipsum dolor sit amet consectetur.</div>
+                </div>
+                <div class="col">
+                    <div class="card">Lorem ipsum dolor sit amet consectetur.</div>
+                </div>
+                <div class="col">
+                    <div class="card">Lorem ipsum dolor sit amet consectetur.</div>
+                </div>
+                <div class="col">
+                    <div class="card">Lorem ipsum dolor sit amet consectetur.</div>
+                </div>
+                <div class="col">
+                    <div class="card">Lorem ipsum dolor sit amet consectetur.</div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
